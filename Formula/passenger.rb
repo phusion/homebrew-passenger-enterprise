d9abb69d15b6c0cf011c72e8137242f80834e94e
Camden Narzt <c.narzt@me.com>
2016-07-24 15:18:18 -0600

    Initial Commit

---
+++ Formula/passenger.rb
@@ -0,0 +1,98 @@
+class Passenger < Formula
+  desc "Enterprise Server for Ruby, Python, and Node.js apps via Apache/NGINX"
+  homepage "https://www.phusionpassenger.com/"
+
+  def self.token
+    filepath = "~/.passenger-enterprise-download-token"
+    begin
+      token = File.read(File.expand_path(filepath))
+    rescue Errno::ENOENT, Errno::EACCES, Errno::EISDIR
+      token = ENV['PASSENGER_TOKEN']
+    ensure
+      while token.nil? or token.empty?
+        puts "passenger token:"
+        token = $stdin.gets
+      end
+      File.write(File.expand_path(filepath),token)
+    end
+    token.chomp
+  end
+
+  version "5.0.29"
+  url "https://www.phusionpassenger.com/orders/download?dir=#{version}&file=passenger-enterprise-server-#{version}.tar.gz", :user => "download:#{Passenger.token}"
+  sha256 "7f02929512bac1362e80bff00a5ec53610f80b2505179e04fd7d06b9c142be1d"
+
+  option "without-apache2-module", "Disable Apache2 module"
+
+  depends_on "pcre"
+  depends_on "openssl"
+  depends_on :macos => :lion
+
+  def install
+    # https://github.com/Homebrew/homebrew-core/pull/1046
+    ENV.delete("SDKROOT")
+
+    rake "apache2" if build.with? "apache2-module"
+    rake "nginx"
+
+    system("/usr/bin/ruby ./bin/passenger-config compile-nginx-engine")
+
+    (libexec/"download_cache").mkpath
+
+    # Fixes https://github.com/phusion/passenger/issues/1288
+    rm_rf "buildout/libev"
+    rm_rf "buildout/libuv"
+    rm_rf "buildout/cache"
+
+    necessary_files = Dir[".editorconfig", "configure", "Rakefile", "README.md", "CONTRIBUTORS",
+      "CONTRIBUTING.md", "LICENSE", "CHANGELOG", "INSTALL.md",
+      "passenger.gemspec", "build", "bin", "doc", "man", "dev", "src",
+      "resources", "buildout"]
+    libexec.mkpath
+    cp_r necessary_files, libexec, :preserve => true
+
+    # Allow Homebrew to create symlinks for the Phusion Passenger commands.
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Ensure that the Phusion Passenger commands can always find their library
+    # files.
+
+    locations_ini = `/usr/bin/ruby ./bin/passenger-config --make-locations-ini --for-native-packaging-method=homebrew`
+    locations_ini.gsub!(/=#{Regexp.escape Dir.pwd}/, "=#{libexec}")
+    (libexec/"src/ruby_supportlib/phusion_passenger/locations.ini").write(locations_ini)
+
+    ruby_libdir = `/usr/bin/ruby ./bin/passenger-config about ruby-libdir`.strip
+    ruby_libdir.gsub!(/^#{Regexp.escape Dir.pwd}/, libexec)
+    system "/usr/bin/ruby", "./dev/install_scripts_bootstrap_code.rb",
+      "--ruby", ruby_libdir, *Dir[libexec/"bin/*"]
+
+    nginx_addon_dir = `/usr/bin/ruby ./bin/passenger-config about nginx-addon-dir`.strip
+    nginx_addon_dir.gsub!(/^#{Regexp.escape Dir.pwd}/, libexec)
+    system "/usr/bin/ruby", "./dev/install_scripts_bootstrap_code.rb",
+      "--nginx-module-config", libexec/"bin", "#{nginx_addon_dir}/config"
+
+    mv libexec/"man", share
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      To activate Phusion Passenger for Nginx, run:
+        brew install nginx --with-passenger
+
+      EOS
+
+    s += <<-EOS.undent if build.with? "apache2-module"
+      To activate Phusion Passenger for Apache, create /etc/apache2/other/passenger.conf:
+        LoadModule passenger_module #{opt_libexec}/buildout/apache2/mod_passenger.so
+        PassengerRoot #{opt_libexec}/src/ruby_supportlib/phusion_passenger/locations.ini
+        PassengerDefaultRuby /usr/bin/ruby
+
+      EOS
+    s
+  end
+
+  test do
+    ruby_libdir = `#{HOMEBREW_PREFIX}/bin/passenger-config --ruby-libdir`.strip
+    assert_equal "#{libexec}/src/ruby_supportlib", ruby_libdir
+  end
+end
