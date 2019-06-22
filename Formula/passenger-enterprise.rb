8b35e5c6e75d6895bbfdd246c0d50a64f63c8c25
Camden Narzt <c.narzt@me.com>
2019-06-21 18:04:08 -0600

    match upstream nginx

--- Formula/passenger-enterprise.rb
+++ Formula/passenger-enterprise.rb
@@ -32,7 +32,7 @@ class PassengerEnterprise < Formula
   conflicts_with "passenger",
     :because => "passenger and passenger-enterprise install the same binaries."
 
-  revision 1
+  revision 2
   patch do
     url "https://github.com/phusion/passenger/commit/09df7df0.patch?full_index=1"
     sha256 "397707a788029f4abac4780d2e04ddba37ec9285b44c9d3e4ff0c91c5121d2b7"
